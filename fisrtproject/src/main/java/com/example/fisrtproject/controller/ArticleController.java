2dee2066300094abad20cc4fb429f1e584263024
yoonezi <jeji3391@naver.com>
2023-09-25 01:58:16 +0900

    16 데이터 삭제하기

--- fisrtproject/src/main/java/com/example/fisrtproject/controller/ArticleController.java
+++ fisrtproject/src/main/java/com/example/fisrtproject/controller/ArticleController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -97,4 +99,22 @@ public class ArticleController {
         return "redirect:/articles/" + articleEntity.getId();
     }
 
+    @GetMapping("articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("delete");
+
+//        1. 삭제 대상을 가져온다
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+//        2. 대상을 삭제한다
+        if (target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제가 완료 되었습니다.");
+        }
+
+//        3. 결과 페이지로 리다이렉트한다
+        return "redirect:/articles";
+
+    }
 }
