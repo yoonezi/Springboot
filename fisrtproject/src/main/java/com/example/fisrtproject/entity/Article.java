ae96d8ec0c9f8a3f4bd21f5b226378e93891b802
yoonezi <jeji3391@naver.com>
2023-09-26 01:12:04 +0900

    17 CRUD와 SQL쿼리_로깅 설정

--- fisrtproject/src/main/java/com/example/fisrtproject/entity/Article.java
+++ fisrtproject/src/main/java/com/example/fisrtproject/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
@@ -18,7 +15,8 @@ import javax.persistence.Id;
 public class Article {
 
     @Id // 대표값을 지정! like a 주민등록번호
-    @GeneratedValue // 1, 2, 3.. 자동 생성 어노테이션!
+//    @GeneratedValue // 1, 2, 3.. 자동 생성 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성해준다.
     private Long id;
 
     @Column
